a6117659c6a0e15e215d820c7471dd414d9f4fd6
Eko450 <78909957+Eko450-eng@users.noreply.github.com>
2024-12-09 15:33:26 +0100

    Update lib.rs

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -166,7 +166,7 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![
-            createdb, addnote, typetext, getnote, deletenote, createnote
+            createdb, addnote, getnote, deletenote, createnote
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
